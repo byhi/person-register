fac9ea71733bd8dd792404bf63e6a9afaac4c90c
byhi <byhisoft@gmail.com>
2024-01-24 12:03:03 +0100

    Init project

---
+++ src/test/java/hu/byhi/personregister/PersonRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.personregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
